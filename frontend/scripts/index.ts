ca8b50bc54aed2633c6187ed478d8357124bedb8
elucid503 <elucid@duck.com>
2025-09-10 00:38:43 -0400

    Uncomitted work; some basic notif api integrations on frontend

--- frontend/scripts/index.ts
+++ frontend/scripts/index.ts
@@ -1,1 +1,57 @@
-console.log("Hello - on Frontend!");
+import $ from "jquery";
+
+// Quick Check if Already Subscribed
+
+const pushMan = ("pushManager" in window ? window.pushManager : null) as PushManager | null;
+
+if (!pushMan) {
+
+    console.warn("Push Manager not supported");
+    $("#subscribe").attr("disabled", "").find("span").text("Push Not Supported");
+
+}
+
+// Notification Subscribe Button
+
+const updateSubscribeButton = async () => {
+
+    const existingSub = await pushMan?.getSubscription().catch(() => { });
+
+    $("#subscribe").attr("mode", existingSub ? "unsubscribe" : "subscribe").find("span").text(`${existingSub ? "Subscribe to" : "Unsubscribe from"} Notifications`);
+
+}
+
+$("#subscribe").on("click", async () => {
+
+    if (!pushMan) return;
+
+    const mode = $("#subscribe").attr("mode") as "subscribe" | "unsubscribe";
+
+    if (mode == "subscribe") {
+
+        const permission = await Notification.requestPermission().catch(() => "denied");
+
+        if (permission != "granted") return alert("You need to allow notifications to subscribe.");
+
+        const newSub = await pushMan.subscribe({
+
+            userVisibleOnly: true,
+            applicationServerKey: "",
+            
+        }).catch(() => null);
+
+        if (!newSub) return alert("Subscription failed.");
+
+        updateSubscribeButton();
+
+    } else {
+
+        const currentSub = await pushMan.getSubscription()
+
+        currentSub?.unsubscribe().catch(() => { });
+
+        updateSubscribeButton();
+        
+    }
+
+})
