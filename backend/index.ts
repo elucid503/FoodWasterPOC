82ac7b49ec930dd0a7ee8b89b262f117e872443f
elucid503 <elucid@duck.com>
2025-09-11 00:00:11 -0400

    Web Push integration

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,13 @@
 import express from "express";
 
+import {
+
+    buildPushPayload,
+    type PushSubscription,
+    type PushMessage,
+  
+} from '@block65/webcrypto-web-push';
+
 import config from "./config.json" assert { type: "json" };
 
 // Server Initialization
@@ -15,6 +23,101 @@ app.use((req, _res, next) => {
     console.log(`${req.method} ${req.url}`);
 
     next();
+
+});
+
+const getSubs = async (): Promise<PushSubscription[]> => {
+
+    return await Bun.file("./subs.json").json().catch(() => []) as PushSubscription[];
+
+}
+
+const addSub = async (sub: PushSubscription) => {
+
+    const existingSubs = await getSubs();
+    existingSubs.push(sub);
+
+    await Bun.write("./subs.json", JSON.stringify(existingSubs, null, 4));
+
+}
+
+const removeSub = async (sub: PushSubscription) => {
+
+    const existingSubs = await getSubs();
+    const filteredSubs = existingSubs.filter(s => s.endpoint != sub.endpoint);
+
+    await Bun.write("./subs.json", JSON.stringify(filteredSubs, null, 4));
+
+};
+
+app.post("/subscribe", express.json(), async (req, res) => {
+
+    const sub = req.body.sub as PushSubscription;
+
+    if (!sub || !sub.endpoint) return res.status(400).end();
+
+    await addSub(sub);
+
+    res.status(201).end();
+
+});
+
+app.post("/notify", express.json(), async (req, res) => {
+
+    // for now, just sends a delayed push notif to all subs after n ms (specified in config)
+    
+    setTimeout(async () => {
+
+        const allSubs = await getSubs();
+
+        const notifPayload: PushMessage = {
+
+            data: {
+
+                "web_push": 8030,
+
+                "notification": {
+
+                    "title":   "Test Notification",
+                    "body":    "This is a test notification from the Food Waster proof-of-concept app. These notifications can be sent at any time, even when the app is closed!",
+                    "silent":  false,
+                    "navigate": "https://previews.sprout.software",
+
+                },
+
+            }
+
+        };
+        
+        allSubs.forEach(async (sub) => {
+
+            const payload = await buildPushPayload(notifPayload, sub, {
+
+                subject: "mailto:paul@paulujlaky.com",
+                publicKey: config.vapid.public,
+                privateKey: config.vapid.private,
+
+            });
+
+            const res = await fetch(sub.endpoint, payload);
+
+            if (res.status == 410 || res.status == 404) {
+
+                console.warn("Removing subscription");
+
+                // subscription is no longer valid
+
+                await removeSub(sub);
+
+            }
+
+            console.log(`Notification sent. Status: ${res.status}`);
+
+        });
+
+    }, config.testing.notificationTimeout);
+
+    res.status(200).end();
 
 });
 
--- frontend/scripts/index.ts
+++ frontend/scripts/index.ts
@@ -11,15 +11,29 @@ if (!pushMan) {
 
 }
 
+// Utils for encoding keys
+
+const urlBase64ToUint8Array = (base64: string) => {
+
+    const padding = "=".repeat((4 - base64.length % 4) % 4);
+  
+    const res = (base64 + padding).replace(/\-/g, "+").replace(/_/g, "/");
+
+    return Uint8Array.from([...atob(res)].map(char => char.charCodeAt(0)));
+    
+}
+
 // Notification Subscribe Button
 
 const updateSubscribeButton = async () => {
 
     const existingSub = await pushMan?.getSubscription().catch(() => { });
 
-    $("#subscribe").attr("mode", existingSub ? "unsubscribe" : "subscribe").find("span").text(`${existingSub ? "Subscribe to" : "Unsubscribe from"} Notifications`);
+    $("#subscribe").attr("mode", existingSub ? "unsubscribe" : "subscribe").find("span").text(`${existingSub ? "Unsubscribe from" : "Subscribe to"} Notifications`);
 
 }
+
+updateSubscribeButton(); // initial state
 
 $("#subscribe").on("click", async () => {
 
@@ -36,11 +50,24 @@ $("#subscribe").on("click", async () => {
         const newSub = await pushMan.subscribe({
 
             userVisibleOnly: true,
-            applicationServerKey: "",
+            applicationServerKey: urlBase64ToUint8Array("BL253OvYzN_eeh7aI3zPIL8_PdvYoxc9Dxj0KcpksRm8o_gXjflIcfj0I6wFFJ5iSPEn00LeE3at1nbSZOAqm6E"), // hard coded here; in config.json on backend
             
         }).catch(() => null);
+        
+        if (!newSub) return alert("Subscription failed. Error: No subscription object.");
 
-        if (!newSub) return alert("Subscription failed.");
+        await fetch("/subscribe", {
+
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+
+            body: JSON.stringify({sub: newSub.toJSON()}), // backend just gets body as sub. no validation for now ;)
+
+        }).then(res => {
+
+            if (!res.ok) throw new Error("Subscription failed. Error: request non-200.");
+
+        }).catch(() => alert("Subscription failed. Error: request failed."));
 
         updateSubscribeButton();
 
@@ -48,10 +75,36 @@ $("#subscribe").on("click", async () => {
 
         const currentSub = await pushMan.getSubscription()
 
-        currentSub?.unsubscribe().catch(() => { });
+        await currentSub?.unsubscribe().catch(() => { });
 
         updateSubscribeButton();
         
     }
 
 })
+
+$("#notify").on("click", async () => {
+
+    const resp = await fetch("/notify", {
+        
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({})
+
+    }).catch(() => {
+
+        alert("Notification scheduling failed. Error: request failed.");
+
+    });
+
+    if (!resp || !resp.ok) return alert("Notification scheduling failed. Error: request non-200.");
+
+    $("#notify").attr("disabled", "").find("span").text("Notification Scheduled");
+
+    setTimeout(() => {
+
+        $("#notify").removeAttr("disabled").find("span").text("Schedule a Notification");
+
+    }, 5_000); // 5s cooldown
+
+});
